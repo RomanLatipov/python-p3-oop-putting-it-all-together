8eacf1af6de40463c0aa57e6b974b48a3f7c2b9c
romanlatipov <romanlatipov@gmail.com>
2024-04-23 14:17:33 -0400

    complete oop ptting it all together

--- debug.py
+++ debug.py
@@ -1,7 +1,8 @@
 import ipdb
 from lib.book import *
+from lib.shoe import *
 
 cradle = Book("Cradle", 200)
 
-if __name__ == '__main__':
-    ipdb.set_trace()
+# if __name__ == '__main__':
+#     ipdb.set_trace()
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand:str, size:int):
+        if type(size) != int:
+            print("size must be an integer")
+        self.brand= brand
+        self.size = size
+        self.condition = "New"
+
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, value:int):
+        if type(value) == int:
+            self._size = value
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        if(self.condition == "Old"):
+            self.condition = "New"
+            print("The shoe has been restored to new condition.")
+        else:
+            print("Your shoe is as good as new!")
